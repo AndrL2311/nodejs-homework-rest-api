df27799a4cfef29adf7431184b3aa21527a0beb7
Andrii Lypovetskyi <li_andr@ukr.net>
2021-12-24 22:59:48 +0200

    Update contact.js

--- model/contact.js
+++ model/contact.js
@@ -8,22 +8,25 @@ const joiSchema = Joi.object({
   favorite: Joi.bool(),
 });
 
-const contactSchema = Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Contact = model("contact", contactSchema);
 
