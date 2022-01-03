8df63385024319ee44ece6bb21b26e5aefbc1bf8
Andrii Lypovetskyi <li_andr@ukr.net>
2022-01-03 14:32:48 +0200

    /logout

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -71,11 +71,17 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-router.get("/current", authenticate, async (req, res, next) => {
+router.get("/current", authenticate, async (req, res) => {
   const { email, subscription } = req.user;
   res.json({
     user: { email, subscription },
   });
+});
+
+router.get("/logout", authenticate, async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: null });
+  res.status(204).send();
 });
 
 module.exports = router;
