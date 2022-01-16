e148efdc1a12305eee2363182d6a068e8f4d641c
Andrii Lypovetskyi <li_andr@ukr.net>
2022-01-16 12:34:39 +0200

    Проверяем раздачу статики

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const authRouter = require("./routes/api/auth");
 // const userRouter = require("./routes/api/users");
 const contactsRouter = require("./routes/api/contacts");
+// const userAvatar = require("./routes/api/users");
 
 const app = express();
 
@@ -14,6 +15,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/users", authRouter);
 // app.use("/api/users");
